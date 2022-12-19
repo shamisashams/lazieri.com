288772e146000dea0917e525626c33dd8442cb48
Alex Zhukov <alex3dfree@gmail.com>
2022-12-19 12:18:24 +0400

    update fix 21

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
         $images = [];
         foreach ($page->sections as $sections){
             if($sections->file){
-                $images[] = asset($sections->file->thumb_full_url());
+                $images[] = asset($sections->file->thumb_url_full);
             } else {
                 $images[] = null;
             }
